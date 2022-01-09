5ac19f722bef19fda3bb64c52a9c6bebfd87a315
grm27 <gabrielraul.marini@mail.polimi.it>
2022-01-09 10:56:26 +0100

    first draft of launch script

---
+++ Test-Script/test_launch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CONFLICT_TYPE='SingleLine'
+CONFLICT_DATASET_FILE='conflict_dataset.csv'
+CONFLICT_INDEX_FILE='conflict_index.json'
+PERFORMANCE_FILE='performance.txt'
+REGEX_REPLACE_INDEX_FILE='regex_replace_index.json'
+REGEX_REPLACE_RESULT_FILE='regex_replace_result.txt'
+REGEX_REPLACE_TREE_INDEX_DIR='regex_replace_tree_index'
+
+for file in ../Data/$CONFLICT_TYPE/*; do
+  f=${file##*/}
+  filename="${f%.*}"
+
+  ./almost-rerere "$file"
+
+  result_dir=".git/rr-cache/results/$filename"
+  mkdir -p "$result_dir"
+
+  mv ".git/rr-cache/$CONFLICT_DATASET_FILE" "$result_dir/$CONFLICT_DATASET_FILE"
+  mv ".git/rr-cache/$CONFLICT_INDEX_FILE" "$result_dir/$CONFLICT_INDEX_FILE"
+  mv ".git/rr-cache/$PERFORMANCE_FILE" "$result_dir/$PERFORMANCE_FILE"
+  mv ".git/rr-cache/$REGEX_REPLACE_INDEX_FILE" "$result_dir/$REGEX_REPLACE_INDEX_FILE"
+  mv ".git/rr-cache/$REGEX_REPLACE_RESULT_FILE" "$result_dir/$REGEX_REPLACE_RESULT_FILE"
+
+  rm -r -f .git/rr-cache/$REGEX_REPLACE_TREE_INDEX_DIR/*
+done
